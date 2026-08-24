8a439d56aa4fde650ca32239a9b1afa167deff7d
contributerMe <nmaslekar777@gmail.com>
2026-08-24 10:24:36 +0530

    dev performance enhnacements using shapely and use of polyine to decomress the route from osrm

--- api/services/routing.py
+++ api/services/routing.py
@@ -7,8 +7,9 @@ def get_route(start_coords, end_coords):
     """
     start_lon, start_lat = start_coords
     end_lon, end_lat = end_coords
-    
-    url = f"http://router.project-osrm.org/route/v1/driving/{start_lon},{start_lat};{end_lon},{end_lat}?overview=simplified&geometries=geojson"
+    url = f"http://router.project-osrm.org/route/v1/driving/{start_lon},{start_lat};{end_lon},{end_lat}?overview=full&geometries=polyline"
+# use overview = simplified or  full based on performance requirements 
+    # url = f"http://router.project-osrm.org/route/v1/driving/{start_lon},{start_lat};{end_lon},{end_lat}?overview=simplified&geometries=geojson"
     response = requests.get(url, timeout=10)
     response.raise_for_status()
     data = response.json()
@@ -16,8 +17,16 @@ def get_route(start_coords, end_coords):
     if data.get('code') != 'Ok' or not data.get('routes'):
         return None, 0
         
+    import polyline
     route = data['routes'][0]
-    geometry = route['geometry'] # dict with type: LineString, coordinates: [[lon, lat], ...]
+    
+    # Decode polyline string. polyline.decode returns [(lat, lon), ...]
+    decoded_coords = [[lon, lat] for lat, lon in polyline.decode(route['geometry'])]
+    
+    geometry = {
+        "type": "LineString",
+        "coordinates": decoded_coords
+    }
     distance = route['distance'] # meters
     
     return geometry, distance
